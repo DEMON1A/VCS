1bc90d42339afffdc9768d5307e91c2c6497b80c
Mohamed Dief <mdaif1332@gmail.com>
2021-03-02 14:45:27 +0200

    Update scannerBase.py

--- scanner/scannerBase.py
+++ scanner/scannerBase.py
@@ -1,5 +1,6 @@
 from os import listdir
 from scanner.scansStarter import startScan
+from scanner.phpStarter import phpStart
 
 from utils.showMessage import showError
 
@@ -9,6 +10,7 @@ def scannerBase(filePath , Language):
     if Language == "all":
         modulesList = listdir('modules/')
         __MODULES__ = []
+        __PHP_MODULES__ = []
 
         for languageDir in modulesList:
             languageModules = listdir(f'modules/{languageDir}')
@@ -16,16 +18,21 @@ def scannerBase(filePath , Language):
             for singleFile in languageModules:
                 if singleFile.endswith('.py'):
                     __MODULES__.append(f'{languageDir}.{singleFile}')
+                elif singleFile.endswith('.php'):
+                    __PHP_MODULES__.append(f'{languageDir}/{singleFile}')
                 else:
                     pass
     else:
         if Language in languagesList:
             modulesList = listdir(f'modules/{Language}/')
             __MODULES__ = []
+            __PHP_MODULES__ = []
 
             for singleModule in modulesList:
-                if singleFile.endswith('.py'):
-                    __MODULES__.append(singleModule)
+                if singleModule.endswith('.py'):
+                    __MODULES__.append(f'{singleModule}')
+                elif singleModule.endswith('.php'):
+                    __PHP_MODULES__.append(f'{Language}/{singleModule}')
                 else:
                     pass
         else:
@@ -33,3 +40,4 @@ def scannerBase(filePath , Language):
             exit()
 
     startScan(filePath=filePath , Modules=__MODULES__)
+    phpStart(filePath=filePath , phpScripts=__PHP_MODULES__)

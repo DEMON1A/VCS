c17049c8aeed5d606e1977e15ed51ff55199b1d2
Mohamed Dief <mdaif1332@gmail.com>
2021-03-23 17:17:27 +0200

    Update listFiles.py

--- utils/listFiles.py
+++ utils/listFiles.py
@@ -1,5 +1,25 @@
-from os import listdir
+from os import scandir
 
 def listFiles(dirPath):
-    files = listdir(f"{dirPath}/")
-    return files
+    pathsList = []
+    dirsList = []
+
+    filesList = scandir(f"{dirPath}/")
+    for singleFile in filesList:
+        if singleFile.is_dir() == True:
+            dirsList.append(singleFile.path)
+        else:
+            pathsList.append(singleFile.path)
+
+    while len(dirsList) != 0:
+        for singleDir in dirsList:
+            filesList = scandir(f"{singleDir}")
+            for singleFile in filesList:
+                if singleFile.is_dir() == True:
+                    dirsList.append(singleFile.path)
+                else:
+                    pathsList.append(singleFile.path)
+
+            dirsList.remove(singleDir)
+
+    return pathsList
